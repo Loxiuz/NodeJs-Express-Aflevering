23df71607b49d2cc5aa2a871eaaebfc10d9aebe1
Malte <91071750+Loxiuz@users.noreply.github.com>
2023-09-04 21:24:08 +0200

    html og data

--- back-end/app.js
+++ back-end/app.js
@@ -2,8 +2,14 @@ import express from "express";
 import fs from "fs/promises";
 
 const app = express();
+app.use(express.json());
 
 const port = 8080;
 app.listen(port, () => {
-  console.log(`App kører på http://localhost:${port}`);
+  console.log(`App running on http://localhost:${port}`);
+});
+
+//Homepage
+app.get("/", (req, res) => {
+  res.send("This is the homepage");
 });
---
+++ front-end/homepage/homepage-script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+window.addEventListener("load", start);
+
+function start() {
+  document
+    .querySelector("#favorites-btn")
+    .addEventListener("click", favoritesBtnClicked);
+  document
+    .querySelector("#create-btn")
+    .addEventListener("click", createArtistClicked);
+}
+
+function favoritesBtnClicked() {}
+
+function createArtistClicked() {}
+
+function updateArtistClicked() {}
+
+function deleteArtistClicked() {}
